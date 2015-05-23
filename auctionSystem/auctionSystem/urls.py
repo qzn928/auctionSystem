0663500421c24f96a7c7db828298699ef581309f
sarach <zhangyahui110@126.com>
2015-05-23 10:22:09 +0800

    daoluan

--- auctionSystem/auctionSystem/urls.py
+++ auctionSystem/auctionSystem/urls.py
@@ -2,18 +2,18 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 
-import xadmin
-xadmin.autodiscover()
+#import xadmin
+#xadmin.autodiscover()
 #
-from xadmin.plugins import xversion
-xversion.register_models()
+#from xadmin.plugins import xversion
+#xversion.register_models()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'auctionSystem.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'xadmin/', include(xadmin.site.urls)),
+  #  url(r'xadmin/', include(xadmin.site.urls)),
 )
 
 if settings.DEBUG: #在开发时使用django来返回静态文件
