3e408b3790522381c6f953b0ce7c8eb7b0e97fd8
qzn928 <qzn928@163.com>
2015-11-20 11:55:11 +0800

    注释xadmin相关模块

--- auctionSystem/auctionSystem/settings.py
+++ auctionSystem/auctionSystem/settings.py
@@ -31,16 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-  #  'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'auctionweb',
-    'xadmin',
-    'crispy_forms',
-    'reversion'
+    #'xadmin',
+    #'crispy_forms',
+    #'reversion'
 )
 
 MIDDLEWARE_CLASSES = (
